605334bf21214d6ce7e80b7ca238d00bb5b73276
pancake <pancake@nopcode.org>
2026-01-19 17:18:54 +0100

    Do the r2plugin build only the unified core one

--- r2plugin/asm/deps.mk
+++
@@ -1,5 +0,0 @@
-HBC_ASM_WD=$(LIBR)/plugs/p/r2hermes
-CFLAGS+=-I$(HBC_ASM_WD)/include
-HBC_ASM_OBJ=$(HBC_ASM_WD)/src/r2/asm_hbc.o
-LDFLAGS+=$(HBC_ASM_WD)/build/libhbc.a
-EXTERNAL_STATIC_OBJS+=$(HBC_ASM_OBJ)
--- r2plugin/bin/deps.mk
+++
@@ -1,5 +0,0 @@
-HBC_BIN_WD=$(LIBR)/plugs/p/r2hermes
-CFLAGS+=-I$(LIBR)/plugs/p/r2hermes/include
-HBC_BIN_OBJ=$(HBC_BIN_WD)/src/r2/bin_hbc.o
-LDFLAGS+=$(HBC_BIN_WD)/build/libhbc.a
-EXTERNAL_STATIC_OBJS+=$(HBC_BIN_OBJ)
